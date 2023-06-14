f1083349da4b8eded2c6912b00df51c7beb30a04
gilgonzalez <fragilgon@gmail.com>
2023-06-14 22:04:24 +0200

    0%

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text> HOME SCREEN</Text>
+      <Icon
+        name="star-outline"
+        size={50}
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
